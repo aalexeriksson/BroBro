4647e8aef32ad444e13a38758d27fb60b9a1fabb
bernstgunnar <phieri-5@student.ltu.se>
2019-07-01 18:42:09 +0200

    testar att lägga till ett as projekt, se om du kan öppna skiten

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //Här man skriver allt skit, identifieara med layout knappar och skit.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
