1abab1f1ae2454b98eedddc78bbd780d2ef8cefa
bernstgunnar <phieri-5@student.ltu.se>
2019-07-02 20:00:33 +0200

    nu :)

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -9,11 +9,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val question = "Vad är 2+2?"
 
-        val correctAnswer = 4
-        var curAnswer = "";
 
+        //Val är som final , var är modifier bar.
+        var question = "Vad är 2+2?";
+        var correctAnswer = "4";
+        var curAnswer = " ";
+
+
+        //Final
         val subAnswer = findViewById<Button>(R.id.buttonAnswer);
         val selQuestOne = findViewById<Button>(R.id.buttonSelectOne);
         val selQuestTwo = findViewById<Button>(R.id.buttonSelectTwo);
@@ -26,20 +30,20 @@ class MainActivity : AppCompatActivity() {
 
 
         selQuestOne.setOnClickListener {
-            var curAnswer = selQuestOne.text
+            curAnswer = selQuestOne.text.toString();
         }
         selQuestTwo.setOnClickListener {
-            var curAnswer = selQuestTwo.text
+            curAnswer = selQuestTwo.text.toString();
         }
         selQuestThree.setOnClickListener {
-            var curAnswer = selQuestThree.text
+            curAnswer = selQuestThree.text.toString();
         }
         selQuestFour.setOnClickListener {
-            var curAnswer = selQuestFour.text
+            curAnswer = selQuestFour.text.toString();
         }
 
         subAnswer.setOnClickListener {
-            if (curAnswer = correctAnswer){
+            if (curAnswer == correctAnswer){
                 //SetTextTill CORRECT NOICE!
             }else{
                 //SetTextTill YOU SÖK.
